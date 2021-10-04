6fb2da8d353e050a2ffa67da3032dba5ddfb1abe
Yohans Nascimento <51249906+YohansN@users.noreply.github.com>
2021-10-04 18:04:22 -0300

    Update GeradorDeSenhas.py

--- GeradorDeSenhas.py
+++ GeradorDeSenhas.py
@@ -8,7 +8,7 @@ if type(quantidade_caracteres) != int:
 
 #Listas de caracteres que podem ser usados:
 caracteres = ['1', '2', '3', '4', '5', '6', '7', '8', '9',\
-    'a','b','c','d','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',\
+    'a','b','c','d','f','e','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',\
         'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',\
             '$','&','_','-']
 
